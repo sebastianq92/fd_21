536b6d5d9377f770711d2cf06bf7cf6bbe1b0f87
sebastianq92 <55534624+sebastianq92@users.noreply.github.com>
2021-03-02 18:34:32 -0600

    Update 2_DDL_Tablas_Agregadas.sql

--- 2_DDL_Tablas_Agregadas.sql
+++ 2_DDL_Tablas_Agregadas.sql
@@ -1,7 +1,7 @@
 
 -- tabla agregada con cantidad de rentas y teimpo promedio de renta calculados en la última fecha de corte
 -- por auto y por agencias de renta y devolucion
-create table rentas_por_auto_agencia_ultimo(
+create table rentas_por_auto_agencia(
   auto_id integer NOT NULL ,
   agencia_renta integer NOT NULL ,
   agencia_devolucion integer NOT NULL ,
